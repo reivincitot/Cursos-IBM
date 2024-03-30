6a5019a1001da6e83313a39830c5b674fe8ec1c8
reivincitot <reivincitot@gmail.com>
2024-03-30 01:00:53 -0300

    Lab model development

--- Data science with R/Data Visualization With R/mtcars.R
+++ Data science with R/Data Visualization With R/mtcars.R
@@ -26,3 +26,23 @@ barp <- barp + theme(
   axis.title.y = element_blank(),
   panel.background = element_blank()) + labs(y="Carburetors")
 print(barp)
+
+#-----------------------------------------------------------------
+ggplot(data=mtcars,aes(x=mpg, y=wt)) +
+  geom_point(shape=1)
+############## dependiendo de los valores cambiara la forma de los puntos en el scatterplot
+mtcars$cylFactor <- factor(mtcars$cyl)
+
+ggplot(data = mtcars, aes(x=mpg,y=wt, shape= cylFactor)) +
+  geom_point()
+############## dependiendo de los valores cambiara de color de los puntos en el scatter plot
+ggplot(data = mtcars , aes(x=mpg, y= wt, color=cylFactor)) +
+  geom_point()
+##########
+ggplot(data = mtcars, aes(x=mpg,y=wt, shape=cylFactor)) +
+  geom_point() +
+  labs(title= "Scatterplot", x="Miles per Gallon", y="Weight")
+############
+ggplot(data=mtcars, aes(x=mpg,y=wt,color=cylFactor)) +
+  geom_point(shape=19) +
+  labs(title="Scatterplot", x="Miles per Gallon", y="Weight", colour="Cylinders")
